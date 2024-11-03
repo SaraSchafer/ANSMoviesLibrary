6e913ad73f457d6da50e1073277dab0769447406
NickOption <netschke@umich.edu>
2024-11-03 17:57:32 -0500

    Update App.js

--- ensmovielibrary/src/App.js
+++ ensmovielibrary/src/App.js
@@ -7,23 +7,26 @@ import Profile from "./pages/profile";
 import Login from "./pages/login";
 import UpdateMovies from "./pages/updateMovies";
 import AllMovies from "./pages/allMovies";
+import NewAccount from "./pages/newAccount";  // Import NewAccount component
+
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/updateMovies" element={<UpdateMovies />} />
-					<Route path="/movies" element={<AllMovies />} />
-				</Routes>
-			</Router>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/updateMovies" element={<UpdateMovies />} />
+                    <Route path="/movies" element={<AllMovies />} />
+                    <Route path="/signup" element={<NewAccount />} />  {/* Added signup route */}
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
