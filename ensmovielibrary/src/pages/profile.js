13f7b3d43f7068f8f27123bb5e02ad524d1662d2
Sara Schafer <sarsc@umich.edu>
2024-11-12 18:33:08 -0500

    profileAndPayment - added profile page

--- ensmovielibrary/src/pages/profile.js
+++ ensmovielibrary/src/pages/profile.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/App.css";
+import profilePicture from "../images/profilePicture.png";
 
 const Profile = () => {
+	const [currentPassword, setCurrentPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [retypeNewPassword, setRetypeNewPassword] = useState("");
+	const user = "John Doe";
+
 	return (
 		<div>
 			<h1>Profile</h1>
+			<div className="profileBox">
+				<div className="profileContent">
+					<img
+						src={profilePicture}
+						alt="PP"
+						id="profileImg"
+						className="profile-picture column column1"
+					/>
+					<div className="column column2">
+						<h2>Username: {user}</h2>
+						<div className="profilePassword">
+							<label htmlFor="currentPassword">Current Password:</label>
+							<input
+								type="password"
+								id="currentPassword"
+								value={currentPassword}
+								onChange={(e) => setCurrentPassword(e.target.value)}
+								required
+							/>
+						</div>
+						<div className="profilePassword">
+							<label htmlFor="newPassword">New Password:</label>
+							<input
+								type="password"
+								id="newPassword"
+								value={newPassword}
+								onChange={(e) => setNewPassword(e.target.value)}
+							/>
+						</div>
+						<div className="profilePassword">
+							<label htmlFor="retypeNewPassword">Retype New Password:</label>
+							<input
+								type="password"
+								id="retypeNewPassword"
+								value={retypeNewPassword}
+								onChange={(e) => setRetypeNewPassword(e.target.value)}
+								required
+							/>
+						</div>
+
+						<button
+							className="profileButton"
+							onClick={() => {
+								if (newPassword === retypeNewPassword) {
+									alert("Password changed successfully");
+								} else {
+									alert("Passwords do not match");
+								}
+							}}>
+							Change Password
+						</button>
+						<br></br>
+						<button
+							className="profileButton"
+							onClick={() => {
+								alert("Hello World");
+							}}>
+							Update Card
+						</button>
+						<br></br>
+						<button
+							className="profileButton"
+							onClick={() => {
+								alert("Minecrafting");
+							}}>
+							Change Profile Picture
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
